193acc630ee56d07010118307136b3e028e5a50d
Benji Altman <benji@altmansoftwaredesign.com>
2021-10-23 15:21:49 -0500

    Simplification, just a bool wrapper now. Maybe someday include

--- Gerk.LightweightCancellationToken/LightCancellationToken.cs
+++ Gerk.LightweightCancellationToken/LightCancellationToken.cs
@@ -1,76 +1,28 @@
-﻿using System;
+﻿using Gerk.AsyncThen;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Gerk.LightweightCancellationToken
 {
-	internal class CancellationCallbackDisposer : IDisposable
-	{
-		private bool isDisposed;
-		public bool IsDisposed => Volatile.Read(ref isDisposed);
-
-		public CancellationCallbackDisposer(bool disposed)
-		{
-			isDisposed = disposed;
-		}
-
-		public void Dispose()
-		{
-			Volatile.Write(ref isDisposed, true);
-		}
-	}
-
 	public class LightweightCancellationToken
 	{
-		internal bool cancelled;
-		private readonly object onCancelAppendLock = new object();
-		private List<Action> onCancel;
-
-		internal CancellationCallbackDisposer OnCancellationEvent(Action callback)
-		{
-			var isCanclled = Volatile.Read(ref cancelled);
-			var disposer = new CancellationCallbackDisposer(isCanclled);
-			if (isCanclled)
-				callback();
-			else
-			{
-				void wrapperCallback()
-				{
-					if (!disposer.IsDisposed)
-						callback();
-				}
-
-				lock (onCancelAppendLock)
-				{
-					onCancel.Add(wrapperCallback);
-				}
-			}
-			return disposer;
-		}
+		internal volatile bool cancelled;
 
 		internal void Cancel()
 		{
-			Volatile.Write(ref cancelled, true);
-			foreach (var callback in onCancel)
-			{
-				callback();
-			}
+			cancelled = true;
 		}
 
-		public LightweightCancellationToken()
-		{
-			cancelled = false;
-		}
-
-		public LightweightCancellationToken(bool cancelled)
+		public LightweightCancellationToken(bool cancelled = false)
 		{
 			this.cancelled = cancelled;
 		}
 
 		public LightweightCancellationToken(TimeSpan timeSpan)
 		{
-			Task.Delay(timeSpan).Then
+			Task.Delay(timeSpan).Then(Cancel);
 		}
 	}
 }
